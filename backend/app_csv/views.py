b6b68a3f79bc6369941b0c23242342a04dc0eaab
Mateo Lamas <91668903+mateolamas@users.noreply.github.com>
2024-02-17 19:24:17 +0100

    Modelado el GET en backend

--- backend/app_csv/views.py
+++ backend/app_csv/views.py
@@ -6,9 +6,15 @@ import pandas as pd
 import requests
 from datetime import datetime
 
-
 df = pd.read_csv("../csv/electrodatos.csv")
 consumo_por_hora = df.groupby(df['Hora'])['Consumo'].mean()
+
+# MockUp
+csv_1 = {'fecha' : '14/05/2022', 'consumo_total':'1.765'}
+csv_2 = {'fecha' : '23/12/2019', 'consumo_total':'1.433'}
+csv_3 = {'fecha' : '01/06/2015', 'consumo_total':'3.332'}
+csv_4 = {'fecha' : '18/11/2023', 'consumo_total':'2.566'}
+csv_user = {}
 
 
 def llamada_API(dia):
@@ -30,6 +36,14 @@ def llamada_API(dia):
 
 @csrf_exempt
 def index(request):
+    global csv_user
+
+    if request.method == 'GET':
+        
+        lista_datos = [csv_user, csv_1, csv_2, csv_3]
+
+        return JsonResponse(lista_datos, safe=False, status=200)
+
     if request.method == 'POST':
         
         zone = request.POST.get('zone')
@@ -42,11 +56,15 @@ def index(request):
         df_cut = df[['Fecha','Hora','Consumo_KWh']]
 
         df_cut = df_cut.merge(consumo_por_hora, how='left', left_on='Hora', right_index=True)
- 
+
         l = llamada_API(df['Fecha'][0])
 
+        fecha = df['Fecha'][0]
+        consumo_total = (pd.to_numeric(df['Consumo_KWh'])).sum()
+        
+        csv_user = {'fecha': fecha, 'consumo_total': consumo_total}
+
         df_cut['Coste'] = l * df_cut['Consumo_KWh'].astype(float)
-        print(df_cut)
 
         json_data = df_cut.to_json(orient='records')
 
